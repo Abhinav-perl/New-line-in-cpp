13c93f28ab1a75a873a66fc24a0b1840d58223f7
abhinav-perl <abhinavkumar6182@gmail.com>
2024-09-02 23:07:04 +0530

    Using new line in different way

---
+++ newline.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*using namespace std;
+
+int main(){
+
+cout << "Look how we use different way to use newline" << endl;
+cout << "2nd way is this way" << "\n";
+cout << "now let's look using std::"; 
+
+return 0;
+}*/
+
+int main() {
+
+std::cout << "understand it" << std::endl;
+std::cout << "this is easy \n";
+std::cout << "isn't?";
+}
